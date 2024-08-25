f869b03bb9cb69dd5c908901e3202c1bff974fe2
lucas4lves <zombnfl@gmail.com>
2024-08-25 12:09:41 -0300

    Feat: location independent and uppercase name for def

--- new-header.sh
+++ new-header.sh
@@ -1,12 +1,21 @@
 #! /bin/bash 
 
-header_file_name=$1
-header_name=$2
-dest_folder=$3
+base_folder=$(pwd)
+
+header_name=$1
+header_file_name="$header_name.h"
+header_def_name="${header_name^^}_H"
+
+if [ -d "$base_folder/headers" ]; then
+	dest_folder=$base_folder/headers
+else
+	mkdir $base_folder/headers
+	dest_folder=$base_folder/headers
+fi 
 
 cat <<EOF >$dest_folder/$header_file_name
-#ifndef $header_name
-#define $header_name
+#ifndef $header_def_name
+#define $header_def_name
 
 #endif
 EOF
